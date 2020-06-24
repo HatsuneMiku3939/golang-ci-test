4bac7d61587c468f7946428ed2a11e9c98cdd605
dmmgames-kim-seungsu <kim-seungsu@dmm.com>
2020-06-24 12:10:37 +0900

    Update

--- main.go
+++ main.go
@@ -6,9 +6,14 @@ func Add(a int, b int) int {
 	return a + b
 }
 
+func Sub(a int, b int) int {
+	return a - b
+}
+
 func main() {
 	a := 3
 	b := 9
 
-	fmt.Printf("TestAdd(%d, %d) = %d\n", a, b, Add(a, b))
+	fmt.Printf("Add(%d, %d) = %d\n", a, b, Add(a, b))
+	fmt.Printf("Sub(%d, %d) = %d\n", a, b, Sub(a, b))
 }
--- main_test.go
+++ main_test.go
@@ -21,3 +21,19 @@ func TestAdd(t *testing.T) {
 		assert.Equalf(t, c.S, s, "Add(%d, %d) should be %d, but %d", c.A, c.B, c.S, s)
 	}
 }
+
+func TestSub(t *testing.T) {
+	cases := []struct {
+		A int
+		B int
+		S int
+	}{
+		{A: 3, B: 9, S: -6},
+		{A: 9, B: 3, S: 6},
+	}
+
+	for _, c := range cases {
+		s := Sub(c.A, c.B)
+		assert.Equalf(t, c.S, s, "Sub(%d, %d) should be %d, but %d", c.A, c.B, c.S, s)
+	}
+}
